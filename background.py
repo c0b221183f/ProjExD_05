2ba8a13b7c62664824f78c88150f5647474c2ec6
teratti <c0b22106d3@edu.teu.ac.jp>
2023-07-11 17:43:28 +0900

    背景クラス

--- background.py
+++ background.py
@@ -3,11 +3,12 @@ import pygame as pg
 
 class background(pg.sprite.Sprite):
     def __init__(self):
-        self.image= pg.image.load("ex05/images/sky_img.png")
+        self.image= pg.image.load("ex05/images/sky_img.png")#背景画像を受け取る
+        self.image = pg.transform.rotozoom(self.image, 0, 2.0)#背景画像の大きさ修正
         self.rect = self.image.get_rect()
 
-    def update(self,movevalue:float):
-        self.rect.move_ip(-movevalue,0)
+    def update(self, movevalue:float):
+        self.rect.move_ip(-movevalue, 0)#横方向に画像をキャラに合わせて動かす
 
 
 
