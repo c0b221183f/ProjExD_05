ad36236aa7a2f876ba4b9ce0078b3e1a8e4fe485
tsujiuta <c0b2210244@edu.teu.ac.jp>
2023-07-11 17:55:38 +0900

    main関数変更

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import sys
 
 import pygame as pg
+import character as ch
 
-WITDH = 1600
+WIDTH = 1600
 HEIGHT = 900
+
 
 def main():
     pg.display.set_caption("学長が転んだ")
-    screen = pg.display.set_mode((WITDH, HEIGHT))
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_image = pg.transform.rotozoom(pg.image.load("ex05/images/sky_img.png"), 0, 1.35)
+    character = ch.Character([200, 700])
 
     tmr = 0
     clock = pg.time.Clock()
@@ -16,7 +19,19 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         screen.blit(bg_image, [0, 0])
-    
+        screen.blit(character.image, character.rect)
+        key_lst = pg.key.get_pressed()
+        """
+        # キャラクターと障害物の衝突判定
+        for emy in emys:
+            if  character.rect.collidedict(emy.rect):
+                character
+            else:
+                character = 10
+            character.update(screen)
+        """
+
+        character.calc_mv(key_lst)
         pg.display.update()
         tmr += 1
         clock.tick(50)
